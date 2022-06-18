c6e260ebbe6b19e0466a7f8aafb46aad659f549c
Patricia-Santos <santos.patricia@academico.ifpb.edu.br>
2022-06-18 10:07:31 -0300

    First commit

---
+++ src/pages/cardapio/index.tsx
@@ -0,0 +1,25 @@
+import styles from './Cardapio.module.scss';
+import Buscador from './Buscador';
+import { useState } from 'react';
+import Filtros from './Filtros';
+
+export default function Cardapio() {
+    const [busca, setBusca] = useState("");
+    const [filtro, setFiltro] = useState<number | null>(null);
+    return(
+        <main>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    Açaízeiro
+                </div>
+            </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador busca={busca} setBusca={setBusca} />
+                <div className={styles.cardapio__filtros}>          
+                <Filtros filtro={filtro} setFiltro={setFiltro} />
+                </div>
+            </section>
+        </main>
+    )
+}
