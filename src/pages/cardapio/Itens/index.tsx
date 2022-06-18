1ca452dc477e0f104bd14e4cae0cc4b7d036c8e2
Patricia-Santos <santos.patricia@academico.ifpb.edu.br>
2022-06-18 19:55:06 -0300

    Finalizando cardapio

--- src/pages/cardapio/Itens/index.tsx
+++ src/pages/cardapio/Itens/index.tsx
@@ -1,13 +1,54 @@
-import cardapio from './itens.json';
+import cardapio from './itens.json'; 
 import Item from './Item';
 import styles from './Itens.module.scss';
+import { useState, useEffect } from "react";
 
-export default function Itens() {
-    return (
-        <div className={styles.itens}>
-            {cardapio.map(item => (
-                <Item key={item.id} {...item} />
-            ))}
-        </div>
-    )
+interface Props {
+  busca: string,
+  filtro: number | null,
+  ordenador: string
+}
+
+export default function Itens(props: Props) {
+  const [lista, setLista] = useState(cardapio);
+  const { busca, filtro, ordenador } = props;
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  function testaBusca(title: string) {
+    const regex = new RegExp(busca, 'i');
+    return regex.test(title);
+  }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  function testaFiltro(id: number) {
+    if(filtro !== null) return filtro === id;
+    return true;
+  }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  function ordenar(novaLista: typeof cardapio) {
+    switch(ordenador) {
+      case 'porcao': 
+        return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+      case 'qtd_pessoas':
+        return novaLista.sort((a,b) => a.serving > b.serving ? 1 : -1);
+      case 'preco':
+        return novaLista.sort((a,b) => a.price > b.price ? 1 : -1);
+      default:
+        return novaLista; 
+    }
+  }
+
+  useEffect(() => {
+    const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
+    setLista(ordenar(novaLista));
+  },[busca, filtro, ordenador, ordenar, testaBusca, testaFiltro])
+
+  return (
+    <div className={styles.itens}>
+      {lista.map(item => (
+        <Item key={item.id} {...item} />
+      ))}
+    </div>
+  )
 }
